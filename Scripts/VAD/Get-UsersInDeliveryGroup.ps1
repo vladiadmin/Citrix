738808fbfa9325b0d26659e861430e17ef27c3a9
Vladimir Mihalkov <vladimir.mihalkov@dxc.com>
2021-08-11 17:50:20 +0300

    Upload script Get-UsersInDeliveryGroup.ps1

---
+++ Scripts/VAD/Get-UsersInDeliveryGroup.ps1
@@ -0,0 +1,33 @@
+<# 	Created: 1-Jul-2021 | Script Name: Get-UsersInDeliveryGroup | 
+		Updated: 13-Jul-2021
+		Author: vladimir.mihalkov@dxc.com
+        
+#>
+
+asnp citrix*
+
+$ExportFolder = "$env:USERPROFILE\Documents"
+$ExportPath =  $ExportFolder + "\UsersInDeliveryGroup.csv"
+
+
+if (!(test-path -Path $ExportFolder)) {mkdir $ExportFolder }
+
+$DG = Get-BrokerDesktop -DesktopGroupName 'BAT-WIN10-Persistent' -MaxRecordCount 1000 | select AssociatedUserUPNs, HostedMachineName
+
+Write-Host "HostedMachineName, AssociatedUserUPNs"
+Add-Content -Path $ExportPath "HostedMachineName, AssociatedUserUPNs"
+
+for ($q=0; $q -lt $DG.Length; $q++) {
+$upns = $null
+$hmn = $DG[$q].HostedMachineName
+
+   for ($i=0; $i -lt $DG[$q].AssociatedUserUPNs.length ; $i++) {
+       if ($i -ne $DG[$q].AssociatedUserUPNs.length - 1 )    
+           {$upns += $DG[$q].AssociatedUserUPNs[$i] + ";"}
+       else 
+           {$upns += $DG[$q].AssociatedUserUPNs[$i]}
+   }
+
+   Add-Content -Path $ExportPath "$hmn,$upns"
+   Write-Host "$hmn,$upns"
+}
